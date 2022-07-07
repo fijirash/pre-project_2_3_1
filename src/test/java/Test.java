09ed85b1fa7d30532e559759cd73dc9e76c2ea39
fijirash <fijirash@yandex.ru>
2022-07-07 17:19:10 +0300

    done

--- src/test/java/Test.java
+++
@@ -1,9 +0,0 @@
-import springpp.service.UserService;
-import springpp.service.UserServiceImpl;
-
-public class Test {
-    public static void main(String[] args) {
-//        UserService userService = new UserServiceImpl();
-//        System.out.println(userService.getAllUsers());
-    }
-}
