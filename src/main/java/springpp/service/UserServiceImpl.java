e0d29c188652ff32d23831f5fc368c2ed4275382
fijirash <fijirash@yandex.ru>
2022-07-10 15:56:10 +0300

    correct

--- src/main/java/springpp/service/UserServiceImpl.java
+++ src/main/java/springpp/service/UserServiceImpl.java
@@ -8,7 +8,7 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
+
 public class UserServiceImpl implements UserService{
 
     private final UserDao userDao;
@@ -18,26 +18,31 @@ public class UserServiceImpl implements UserService{
     }
 
     @Override
+    @Transactional
     public List<User> getAllUsers() {
         return userDao.getAllUsers();
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
+    @Transactional
     public void removeUser(int id) {
         userDao.removeUser(id);
     }
 
     @Override
+    @Transactional
     public User getUser(int id) {
         return userDao.getUser(id);
     }
 
     @Override
+    @Transactional
     public void updateUser(int id, User user) {
         userDao.updateUser(id, user);
     }
